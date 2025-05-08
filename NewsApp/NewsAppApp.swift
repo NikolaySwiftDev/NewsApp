accd6756c8b56790f6f88050a87296411c9082a3
rotartechspaceapp <123818046+nikolay1414@users.noreply.github.com>
2025-05-08 17:20:01 +0300

    Init

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Николай on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
